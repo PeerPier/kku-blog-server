7e180c4d23925abdc72846a1f170f8e0687434b2
PeerPier <155882979+PeerPier@users.noreply.github.com>
2024-10-20 17:07:03 +0700

    Reset Password(Admin)

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -1,28 +1,26 @@
-const express = require("express");
-const User = require("../models/user");
-const Admin = require("../models/admin"); // นำเข้าโมเดล Admin
-const router = express.Router();
-const bcrypt = require("bcrypt");
+const express = require("express")
+const User = require("../models/user")
+const Admin = require("../models/admin") // นำเข้าโมเดล Admin
+const router = express.Router()
+const bcrypt = require("bcrypt")
+const { generatePasswordResetToken } = require("../utils/token")
+const sendResetPasswordEmail = require("../mail/reset_password")
 
 router.post("/", async (req, res) => {
-  const { email, newPassword } = req.body;
+  const { email } = req.body
 
   // ตรวจสอบว่าอีเมลและรหัสผ่านใหม่ถูกต้องหรือไม่
   if (!email) {
-    return res.status(400).send({ message: "Email is required" });
-  }
-
-  if (!newPassword) {
-    return res.status(400).send({ message: "New Password is required" });
+    return res.status(400).send({ message: "Email is required" })
   }
 
   try {
     // ค้นหาผู้ใช้ในฐานข้อมูลตามอีเมลในโมเดล User
-    let user = await User.findOne({ email });
-    
+    let user = await User.findOne({ email })
+
     // หากไม่พบผู้ใช้ในโมเดล User ให้ค้นหาในโมเดล Admin
     if (!user) {
-      user = await Admin.findOne({ email });
+      user = await Admin.findOne({ email })
     }
 
     // หากยังไม่พบผู้ใช้ให้ส่งข้อความตอบกลับ
@@ -30,29 +28,39 @@ router.post("/", async (req, res) => {
       return res.status(404).send({
         success: false,
         message: "User not found",
-      });
+      })
     }
 
-    // เข้ารหัสรหัสผ่านใหม่
-    const hashedPassword = await bcrypt.hash(newPassword, 10); // ใช้รอบการเข้ารหัส 10 ครั้ง
+    // // เข้ารหัสรหัสผ่านใหม่
+    // const hashedPassword = await bcrypt.hash(newPassword, 10); // ใช้รอบการเข้ารหัส 10 ครั้ง
 
-    // อัปเดตรหัสผ่านในฐานข้อมูล
-    user.password = hashedPassword;
-    await user.save(); // บันทึกการเปลี่ยนแปลง
+    // // อัปเดตรหัสผ่านในฐานข้อมูล
+    // user.password = hashedPassword;
+    // await user.save(); // บันทึกการเปลี่ยนแปลง
+
+    // สร้างโทเค็นสำหรับ นำไปสร้างลิงก์สำหรับตั้งค่ารหัสผ่านใหม่ แบบกำหนดเวลาหมดอายุ
+    const { token, ref } = await generatePasswordResetToken(user.email)
+
+    // สร้างลิงก์สำหรับตั้งค่ารหัสผ่านใหม่ไปยังอีเมลผู้ใช้
+    const resetPasswordLink = `${process.env.FRONTEND_ENDPOINT}/reset-password/${token}`
+
+    // ส่งอีเมลสำหรับตั้งค่ารหัสผ่านใหม่
+    await sendResetPasswordEmail(user.email, user.username, resetPasswordLink, ref);
 
     // ส่งการตอบกลับสำเร็จ
     res.status(200).send({
       success: true,
       message: "Password reset successfully",
-    });
+      ref,
+    })
   } catch (error) {
-    console.error(error); // บันทึกข้อผิดพลาด
+    console.error(error) // บันทึกข้อผิดพลาด
     res.status(500).send({
       success: false,
       message: "Something went wrong",
       error: error.message, // ส่งข้อความข้อผิดพลาด
-    });
+    })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -1,89 +1,47 @@
 const express = require("express");
-const User = require("../models/user");
-const Admin = require("../models/admin"); // เพิ่มการใช้ Admin model
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const ResetPasswordToken = require("../models/resetPasswordToken");
+const User = require("../models/user");
+const Admin = require("../models/admin");
 const router = express.Router();
 
-const formDatatoSend = (user) => {
-  const access_token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  return {
-    access_token,
-    _id: user._id,
-    profile_picture: user.profile_picture,
-    username: user.username,
-    fullname: user.fullname,
-  };
-};
+router.post("/", async (req, res) => {
+  const { token, password } = req.body;
 
-router.post("/", (req, res) => {
-  let { email, password } = req.body;
+  if (!token || !password) {
+    return res.status(400).json({ success: false, message: "Invalid data" });
+  }
 
-  console.log("Email:", email);
-  console.log("Password:", password);
+  const existingToken = await ResetPasswordToken.findOne({ token })
 
-  Admin.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        return res.status(403).json({ error: "ไม่พบผู้ใช้" });
-      }
-      if (!user.google_auth) {
-        bcrypt.compare(password, user.password, (err, result) => {
-          if (err) {
-            return res
-              .status(403)
-              .json({ error: "เกิดข้อผิดพลาดขณะเข้าสู่ระบบ โปรดลองอีกครั้ง" });
-          }
+  if (!existingToken) {
+    return res.status(400).json({ success: false, message: "Invalid token" })
+  }
 
-          if (!result) {
-            return res.status(403).json({ error: "รหัสผ่านไม่ถูกต้อง" });
-          } else {
-            console.log("formDatatoSend(user)", formDatatoSend(user));
-            return res.status(200).json(formDatatoSend(user));
-          }
-        });
-      } else {
-        return res.status(403).json({
-          error:
-            "บัญชีถูกสร้างด้วยบัญชี Google แล้ว โปรดเข้าสู่ระบบด้วย Google",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err.message);
-      return res.status(500).json({ error: err.message });
-    });
-});
+  if (existingToken.expires < Date.now()) {
+    return res.status(400).json({ success: false, message: "Token expired" })
+  }
 
-router.post("/:type/:id/:token", async (req, res) => {
-  const { id, token, type } = req.params;
-  const { password } = req.body;
-  jwt.verify(token, "jwt_secret_key", (err, decoded) => {
-    if (err) {
-      return res.json({ Status: "Error with token" });
-    } else {
-      bcrypt
-        .hash(password, 10)
-        .then((hash) => {
-          // ตรวจสอบว่าประเภทเป็น user หรือ admin แล้วอัปเดตรหัสผ่าน
-          const model = type === "admin" ? Admin : User; // เลือก model ที่เหมาะสม
-          model
-            .findByIdAndUpdate({ _id: id }, { password: hash })
-            .then((u) => res.send({ Status: "Success" }))
-            .catch((err) => res.send({ Status: err.message }));
-        })
-        .catch((err) => res.send({ Status: err.message }));
+  let role = "user"
+  let user = await User.findOne({ email: existingToken.email })
+
+  if (!user) {
+    user = await Admin.findOne({ email: existingToken.email })
+    role = "admin"
+
+    if (!user) {
+      return res.status(400).json({ success: false, message: "Invalid user" })
     }
-  });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10) // ใช้รอบการเข้ารหัส 10 ครั้ง
+
+  user.password = hashedPassword
+  await user.save()
+
+  await ResetPasswordToken.deleteOne({ token })
+
+  res.status(200).json({ success: true, message: "Password reset successfully", role })
 });
 
-module.exports = router;
-
-// เพิ่มโค้ดทดสอบ JWT ที่นี่
-const testToken = jwt.sign(
-  { userId: "66a0d89f4ee74c78f492432e" }, // ใช้ userId จริง
-  "Jungkook1997", // ใช้ค่า JWT_SECRET จริง
-  { expiresIn: "1d" }
-);
-
-console.log("Test Token:", testToken);
+module.exports = router
